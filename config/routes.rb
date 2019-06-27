94da3356f96803d5d0cdb08a0b3f65ddfe7cc823
Jeremías <jeremiasgutierrez@hotmail.cl>
2019-06-27 00:10:20 -0400

    fix app layout

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,9 @@ Rails.application.routes.draw do
   namespace :user do
     resources :members
     resources :events
-    root to: 'users#index'
+    root to: 'events#index'
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  root to: "user/users#index"
+  root to: "user/events#index"
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_16_073607) do
+ActiveRecord::Schema.define(version: 2019_06_27_040002) do
 
   create_table "user_events", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "title"
@@ -38,6 +38,8 @@ ActiveRecord::Schema.define(version: 2019_06_16_073607) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
+    t.string "lastname"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
