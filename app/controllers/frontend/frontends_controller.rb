2d796431420e1673f41f91f02d3eedbe934bf12a
Jeremías <jimmyguionbajo@gmail.com>
2019-08-29 22:02:49 -0400

    remove talca ciudad cultural from application layout

--- app/controllers/frontend/frontends_controller.rb
+++ app/controllers/frontend/frontends_controller.rb
@@ -2,7 +2,7 @@ class Frontend::FrontendsController < ApplicationController
   layout 'application'
 
   def index
-    @events = User::Event.next_events
+    @events = User::Event.next_events.order(created_at: :desc)
   end
 
   def contact 
