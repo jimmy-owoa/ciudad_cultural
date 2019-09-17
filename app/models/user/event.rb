544c0f44c23bd095c1a085b11cfb4dae71db6e04
Jeremías <jimmyguionbajo@gmail.com>
2019-09-16 23:03:31 -0300

    main image to events

--- app/models/user/event.rb
+++ app/models/user/event.rb
@@ -1,9 +1,10 @@
 class User::Event < ApplicationRecord
-    has_many_attached :images
-    has_one_attached :file
+	has_many_attached :images
+	has_one_attached :file
+	has_one_attached :main_image
 
-    scope :next_events, -> { where("date > ?", Date.today ) }
-    scope :past_events, -> { where("date < ?", Date.today ) }
-    scope :talca_events, -> { where(is_talca: true) }
-
+	scope :next_events, -> { where("date > ?", Date.today ) }
+	scope :past_events, -> { where("date < ?", Date.today ) }
+	scope :talca_events, -> { where(is_talca: true) }
+	
 end
