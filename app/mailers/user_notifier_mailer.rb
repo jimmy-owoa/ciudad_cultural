ec867c3bdc200389baab5972a1de0657a10a9e9a
Jeremías <jimmyguionbajo@gmail.com>
2019-10-14 21:19:25 -0300

    changed mail contact

--- app/mailers/user_notifier_mailer.rb
+++ app/mailers/user_notifier_mailer.rb
@@ -1,10 +1,10 @@
 class UserNotifierMailer < ApplicationMailer
 
-	# send a signup email to the user, pass in the user object that   contains the user's email address
-	def send_contact(name, email, message, phone)
-		@message = message
-		@phone = phone
-		@name = name
-		mail( :to => 'j@laciudadcultural.cl', :subject => 'Contacto web [La Ciudad Cultural]', :from => email)
-	end
+  # send a signup email to the user, pass in the user object that   contains the user's email address
+  def send_contact(name, email, message, phone)
+    @message = message
+    @phone = phone
+    @name = name
+    mail(:to => "contacto@laciudadcultural.cl", :subject => "Contacto web [La Ciudad Cultural]", :from => email)
+  end
 end
