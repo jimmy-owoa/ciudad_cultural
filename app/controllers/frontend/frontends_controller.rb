cfb0148854ac125ce0d2de9d98b318952c99b0eb
Jeremías <jimmyguionbajo@gmail.com>
2019-10-14 21:40:57 -0300

    send emails

--- app/controllers/frontend/frontends_controller.rb
+++ app/controllers/frontend/frontends_controller.rb
@@ -1,21 +1,21 @@
 class Frontend::FrontendsController < ApplicationController
-  layout 'application'
+  layout "application"
 
   def index
     @events = User::Event.all.order(created_at: :desc)
   end
 
-  def contact 
+  def contact
   end
-  
+
   def send_email
     name = params[:name]
     email = params[:email]
     message = params[:message]
     phone = params[:phone]
-    #UserNotifierMailer.send_contact(name, email, message, phone).deliver
+    UserNotifierMailer.send_contact(name, email, message, phone).deliver
     respond_to do |format|
-      format.html { redirect_to root_path, notice: 'Correo enviado exitosamente.' }
+      format.html { redirect_to root_path, notice: "Correo enviado exitosamente." }
     end
   end
 
@@ -24,6 +24,5 @@ class Frontend::FrontendsController < ApplicationController
   end
 
   def work_with_us
-
   end
 end
--- config/environment.rb
+++ config/environment.rb
@@ -1,14 +1,14 @@
 # Load the Rails application.
-require_relative 'application'
+require_relative "application"
 
 ActionMailer::Base.smtp_settings = {
   :user_name => Rails.application.credentials.sendgrid[:user_name],
   :password => Rails.application.credentials.sendgrid[:password],
-  :domain => 'ciudadcultural.cl',
-  :address => 'smtp.sendgrid.net',
+  :domain => "laciudadcultural.cl",
+  :address => "smtp.sendgrid.net",
   :port => 587,
   :authentication => :plain,
-  :enable_starttls_auto => true
+  :enable_starttls_auto => true,
 }
 # Initialize the Rails application.
 Rails.application.initialize!
